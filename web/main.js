71cc96d57d25a7f54d8a47de1c5ae4682efefd6d
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-12 14:19:34 +0100

    .

--- web/main.js
+++ web/main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from '../counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
