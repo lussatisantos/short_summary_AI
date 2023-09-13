48ea3932aed03bcbc52c2f4060902288e859e20b
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-13 18:30:36 +0100

    configurando o back e a rota

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
-import cors from 'cors'
-import express from 'express'
+import cors from "cors"
+import express from "express"
 
 const app = express()
 app.use(cors())
+
+app.get("/summary", (request, response) => {
+  response.send("Ok vou fazer o resumo")
+})
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
