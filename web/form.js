9e97903df823ef9bb39a5a1efde5090bcdc81f88
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-14 17:08:42 +0100

    conectando o formulario com o backend

--- web/form.js
+++ web/form.js
@@ -1,8 +1,23 @@
+import { server } from "./server.js"
+
 const form = document.querySelector("#form")
 const input = document.querySelector("#input")
+const content = document.querySelector("#content")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
+
   const videoURL = input.value
-  console.log('URL DO VIDEO', videoURL)
+
+  if(!videoURL.includes("shorts")){
+    return content.textContent = "Esse video nao parece ser um short."
+  }
+
+  const [_, params] = videoURL.split("/shorts/")
+  const [videoID] = params.split("?si")
+
+  content.textContent = "Obtendo o texto do audio..."
+  server.get("/summary/" + videoID)
+
+  content.textContent = "Realizando o resumo..."
 })
