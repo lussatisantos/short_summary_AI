c597a65b79876b87cd5af42cdf8bf8fb158dde5d
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-18 13:24:03 +0100

    transcricao do audio
    Apos a conversao do video para o audio, o modelo de IA Xenova e Whisper farao transcricao do audio (texto)

--- server/index.js
+++ server/index.js
@@ -11,17 +11,27 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/summary/:id", async (request, response) => {
+  try {
   await download(request.params.id)
   const audioConverted = await convert()
   console.log(audioConverted)
-  const result = await transcribe()
+  const result = await transcribe(audioConverted)
 
   return response.json({ result })
+  } catch(error){
+    console.log(error)
+    return response.json({ error })
+  }
 })
 
 app.post("/summary", async (request, response) => {
+  try {
   const result = await summarize(request.body.text)
   return response.json({ result })
+  } catch(error){
+    console.log(error)
+    return response.json({ error })
+  }
 })
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
--- server/transcribe.js
+++ server/transcribe.js
@@ -1,4 +1,23 @@
+import { pipeline } from "@xenova/transformers"
+
 import { transcriptionExample } from "./utils/transcription.js"
-export async function transcribe(){
-  return transcriptionExample
+
+export async function transcribe(audio){
+ try {
+   //  return transcriptionExample
+
+   console.log("Realizando a transcricao...")
+   const transcribe = await pipeline("automatic-speech-recognition", "Xenova/whisper-small")
+
+   const transcription = await transcribe(audio, {
+    chunk_length_s: 30,
+    stride_length_s: 5,
+    language: "portuguese",
+    task: "transcribe",
+   })
+   console.log("Transcricao finalizado com sucesso...")
+   return transcription?.text.replace("[Musica]","")
+ } catch (error) {
+  throw new Error(error)
+ }
 }
