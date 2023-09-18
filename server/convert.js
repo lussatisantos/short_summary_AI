9cad0752d31f8b248af40187a439fc1584fabdef
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-18 13:10:15 +0100

    conversao do video

---
+++ server/convert.js
@@ -0,0 +1,36 @@
+import fs from "fs"
+import wav from "node-wav"
+import ffmpeg from "fluent-ffmpeg"
+import ffmpegStatic from "ffmpeg-static"
+
+const filePath = "./tmp/audio.mp4"
+const outputPath = filePath.replace(".mp4", ".wav")
+
+export const convert = () =>
+  new Promise((resolve, reject) => {
+    console.log("Convertendo o video...")
+
+    ffmpeg.setFfmpegPath(ffmpegStatic)
+    ffmpeg()
+    .input(filePath)
+    .audioFrequency(16000)
+    .audioChannels(1)
+    .format("wav")
+    .on("end", () => {
+      const file = fs.readFileSync(outputPath)
+      const fildeDecode = wav.decode(file)
+
+      const audiodata = fileDecode.channelData[0]
+      const floatArray = new Float32Array(audioData)
+
+      console.log("Video convertido com sucesso!")
+
+      resolve(floatArray)
+      fs.unlinkSync(outputPath)
+    })
+    .on("error", (error) => {
+      console.log("Erro ao converter o video", error)
+      reject(error)
+    })
+    .save(outputPath)
+  })
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import cors from "cors"
 import express from "express"
 
+import { convert } from "./convert.js"
 import { download } from "./download.js"
 import { transcribe } from "./transcribe.js"
 import { summarize } from "./summarize.js"
@@ -11,6 +12,8 @@ app.use(cors())
 
 app.get("/summary/:id", async (request, response) => {
   await download(request.params.id)
+  const audioConverted = await convert()
+  console.log(audioConverted)
   const result = await transcribe()
 
   return response.json({ result })
