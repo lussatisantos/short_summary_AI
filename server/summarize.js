4cf9d40138249f7017c2ce89787cb9f9bf8017d6
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-09-18 13:43:08 +0100

    resumo do texto

--- server/summarize.js
+++ server/summarize.js
@@ -1,5 +1,23 @@
+import { pipeline } from "@xenova/transformes"
+
 import { summaryExample } from "./utils/summary.js"
 
 export async function summarize(text) {
-  return summaryExample
+  try {
+    //return summaryExample
+
+    console.log("Realizando o resumo...")
+
+    const generator = await pipeline(
+      "summarization", "Xenova/distilbart-cnn-12-6"
+    )
+
+    const output = await generator(text)
+
+    console.log("Resumo concluido com sucesso!")
+    return output[0].summary_text
+  } catch (error) {
+    console.log("Nao foi possivel realizaar o resumo", error)
+    throw new Error(error)
+  }
 }
